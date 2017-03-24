78267d0aea45cfa6525187d08632c14c0a06b28e
lunberg88 <lunberg88@gmail.com>
2017-03-24 18:39:57 +0200

    toprank fixes

--- frontend/controllers/ToprankController.php
+++ frontend/controllers/ToprankController.php
@@ -2,51 +2,18 @@
 
 namespace frontend\controllers;
 
-use Yii;
-use common\models\User;
 use frontend\models\Port;
-use frontend\models\Ship;
-use yii\db\ActiveQuery;
 
 class ToprankController extends \yii\web\Controller
 {
-    /*
     public function actionIndex()
     {
-        $models = User::find()->all();
-        if($models) {
-            foreach ($models as $model) {
-                $userships = Port::findOne(['user_id' => $model->id]);
-            }
-        }
-        $shipname = Ship::findOne([ 'id' => $userships->ship_id]);
-        return $this->render('index', [
-            'models' => $models,
-            'userships' => $userships,
-            'shipname' => $shipname,
-        ]);
-    }
-*/
-    public function actionIndex()
-    {
-        /*
-        $userships = Port::find()
-        ->select('*')
-        ->innerJoin('user', 'port.id = user.id')
-//        ->leftJoin('user', 'port.user_id = user.id')
-//        ->with('port')
-        ->all();
-*/
-        /*
-        echo  '<pre>'.print_r($userships,1).'</pre>';
-        exit();
-        */
- $userships = Port::find()
-     ->joinWith('user')
-     ->all();
 
-echo '<pre>'.print_r($userships,1).'</pre>';
-exit();
+       $userships = Port::find()
+        ->joinWith('user', 'ship')
+        ->orderBy('user.win')
+        ->all();
+
         return $this->render('index', [
            'userships' => $userships,
         ]);
--- frontend/views/toprank/index.php
+++ frontend/views/toprank/index.php
@@ -4,12 +4,39 @@
 <div align="center"><h1><b><?=Yii::t('app', 'Top Rank users') ?>!</b></h1></div>
 <br>
 <p>
-    <?php
-    foreach ($userships as $ships) {
-     //   echo $ships->user_id.'<br>';
-     //   echo $ships->ship_id.'<br>';
-      echo $ships->ship_id.'<br>';
-        echo $ships->username.'<br><hr>';
-    }
-    ?>
+    <div width="90%">
+
+<table align="left" width="90%" cellpadding="5" cellspacing="5" class="toprank">
+        <tr class="toph">
+            <td>#</td>
+            <td><?=Yii::t('app', 'Username') ?></td>
+            <td><?=Yii::t('app', 'Shipname') ?></td>
+            <td><?=Yii::t('app', 'Img') ?></td>
+            <td><?=Yii::t('app', 'Win') ?></td>
+            <td><?=Yii::t('app', 'Lose') ?></td>
+            <td><?=Yii::t('app', 'Draw') ?></td>
+            <td><?=Yii::t('app', 'Mod Gun') ?></td>
+            <td><?=Yii::t('app', 'Mod Tower') ?></td>
+        </tr>
+<?php
+ $i = 1;
+ foreach ($userships as $ships) {
+?>
+    <tr>
+        <td><?=$i?></td>
+        <td><h3><b><?=$ships->user->username ?></b></h3></td>
+        <td><?=$ships->ship->name ?> [<?=$ships->ship->lvl ?>]</td>
+        <td><img width="140" src="../img/ships/<?=$ships->ship->name ?>.png"></td>
+        <td><?=$ships->user->win ?></td>
+        <td><?=$ships->user->lose ?></td>
+        <td><?=$ships->user->draw ?></td>
+        <td><?php if($ships->mod_gun !=1) { echo "<img width='30' src='../img/stock_gun.png'>"; } else { echo "<img title='Modify' width='30' src='../img/mod_gun.png'>"; } ?></td>
+        <td><?php if($ships->mod_tower !=1) { echo "<img width='30' src='../img/stock_tower.png'>"; } else { echo "<img title='Modify' width='30' src='../img/mod_tower.png'>"; } ?></td>
+    </tr>
+<?php
+  ++$i;
+ }
+?>
+</table>
+    </div>
 </p>
